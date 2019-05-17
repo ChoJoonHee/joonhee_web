0e0239bfc96bac7eed604f7706e859d441681613
joonhee <slaxer@hanmail.net>
2019-05-17 10:29:20 +0900

    add Register_loginCtr

--- src/main/java/org/joonhee/book/chap11/Register_login_Controller.java
+++ src/main/java/org/joonhee/book/chap11/Register_login_Controller.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author Jacob
  */
 @Controller
-public class RegisterController {
+public class Register_login_Controller {
 
 	@Autowired
 	MemberDao memberDao;
